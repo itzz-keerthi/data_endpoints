1859f4057828fc9234dad5f5dc19efd2e267b608
Keerthana J <keerthanajothirameshkumar@gmail.com>
2024-05-01 20:22:19 +0530

    json data endpoints

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require('express');
+const router=express.Router();
+const server = express();
+server.listen(8000);
+server.use(router);
+
+router.get('/',(req,res)=>{
+    res.send("Data API endpoints")
+})
+
+router.get('/learnerdata',(req,res)=>{
+    res.sendFile('data/sfl_product.learner_data.json',{root:__dirname});
+})
+
+router.get('/organizationdata',(req,res)=>{
+    res.sendFile('data/sfl_product.organization_data.json',{root:__dirname});
+})
+
+router.get('/programdata',(req,res)=>{
+    res.sendFile('data/sfl_product.program_data.json',{root:__dirname});
+})
+
+router.get('/reportdata',(req,res)=>{
+    res.sendFile('data/sfl_product.report.json',{root:__dirname});
+})
+
+router.get('/sfladmindata',(req,res)=>{
+    res.sendFile('data/sfl_product.sfl_admin_data.json',{root:__dirname});
+})
+
+router.get('/subadmindata',(req,res)=>{
+    res.sendFile('data/sfl_product.sub_admin_data.json',{root:__dirname});
+})
+
+router.get('/superadmindata',(req,res)=>{
+    res.sendFile('data/sfl_product.super_admin_data.json',{root:__dirname});
+})
